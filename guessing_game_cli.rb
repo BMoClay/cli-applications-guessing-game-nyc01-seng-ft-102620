2ba32d054f344d046e95f9dfe6bf0e685615be16
Ben Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-10-18 21:24:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,5 +4,7 @@ def run_guessing_game
   user_number = gets.chomp
     if user_number == "exit"
       puts "/Goodbye!/"
+    elsif user_number != random_number
+      puts "/Sorry! The computer guessed #{random_number}/"
     end
 end
